4a6beaa29c00eb84f8eb013ecb024c8befe4aa60
Eric Junior Nyarko Sampson <ericsampson105@gmail.com>
2022-09-21 23:16:26 +0000

    working on the sidebar
    Signed-off-by: Eric Junior Nyarko Sampson <ericsampson105@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <SideBar />
+  </div>
+</template>
+
+<script setup>
+import SideBar from "./views/SideBar.vue";
+</script>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex my-5">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-5 mr-4"
+    >
+      {{ svgPath }}
+    </svg>
+    <p>{{ itemLabel }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(["svgPath", "itemLabel"]);
+</script>
