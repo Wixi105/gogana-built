545293f7afdf33ec94816c23ffe798b3a4d1a305
Eric Junior Nyarko Sampson <ericsampson105@gmail.com>
2022-09-22 14:38:00 +0000

    working on chart item
    Signed-off-by: Eric Junior Nyarko Sampson <ericsampson105@gmail.com>

--- src/components/ChartItem.vue
+++ src/components/ChartItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="py-7 px-5 border border-gray-500 border-opacity-30 w-1/3 rounded-lg"
+    class="py-7 px-5 border border-gray-500 border-opacity-20 w-1/3 rounded-lg"
   >
     <section>
-      <p class="text-sm">{{ label }}</p>
+      <p class="text-xs pb-1">{{ label }}</p>
       <p class="text-2xl font-bold">{{ price }}</p>
-      <p class="text-sm text-gray-500">{{ desc }}</p>
+      <p class="text-xs text-gray-500 pt-1">{{ desc }}</p>
     </section>
   </div>
 </template>
